252cd2fb4dc909e58f5f4192f78bff6b6a7a306f
the-other-mariana <mariana.avalos.arce@gmail.com>
2022-03-03 20:10:50 -0600

    bash script comments
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- misc/connect-to-serial-port.sh
+++ misc/connect-to-serial-port.sh
@@ -1,6 +1,7 @@
 sudo apt install setserial # UART: unknown means no device 
-sudo setserial -g /dev/ttyS[0123456789]
+sudo setserial -g /dev/ttyS[0123456789] # list tty devices
+dmesg | grep tty # list tty devices
 sudo apt install cu
-sudo chmod 666 /dev/ttyS0
+sudo chmod 666 /dev/ttyS0 # if cu command outputs: Line in use
 sudo cu -l /dev/ttyS0 -s 115200 # out: Connected
 ~. # disconnect
